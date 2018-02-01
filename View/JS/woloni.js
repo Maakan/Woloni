73993e258ece990fd53501516a1d09d22dd2828c
Aliou Makan <makanaliou@gmail.com>
2018-02-01 02:52:50 +0100

    Ajout de commentaire et suppression inutile

--- View/JS/woloni.js
+++ View/JS/woloni.js
@@ -1,14 +1,24 @@
-angular.module('myApp', []).controller('namesCtrl', function($scope) {
+//Déclaration de l'application
+var app = angular.module('myApp', ["ngRoute"]);
+
+//Déclaration du controlleur
+app.controller('namesCtrl', function($scope) {
+
+    //Définition des restaurants et leurs attributs
     $scope.restaurants = [
-        {name:'Le Relax',minimum:2000, price:'€', time:'', livraison:'1500', url:'../IMAGE/relax.jpg'},
-        {name:'Amandine',minimum:1500, price:'€', time:'', livraison:'1500', url:'../IMAGE/amandine.jpg'},
-        {name:"L'olympien",minimum:5000, price:'€€€', time:'', livraison:'1500', url:'../IMAGE/olympien.jpg'},
-        {name:'Bobsi',minimum:1500, price:'€', time:'', livraison:'1500', url:'../IMAGE/bobsi.jpg'},
-        {name:"Guido's Pizza",minimum:5000, price:'€€', time:'', livraison:'1500', url:'../IMAGE/guidos.jpg'},
-        {name:'Sigma Burger',minimum:2500, price:'€€', time:'', livraison:'1500', url:'../IMAGE/sigma.jpg'},
+        {name:'Le Relax',minimum:2000, price:'€', time:'', livraison:'1500', url:'../IMAGE/relax.jpg', lien :'../HTML/olympien.html'},
+        {name:'Amandine',minimum:1500, price:'€', time:'', livraison:'1500', url:'../IMAGE/amandine.jpg', lien :'../HTML/olympien.html'},
+        {name:"L'olympien",minimum:5000, price:'€€€', time:'', livraison:'1500', url:'../IMAGE/olympien.jpg', lien :'../HTML/olympien.html'},
+        {name:'Bobsi',minimum:1500, price:'€', time:'', livraison:'1500', url:'../IMAGE/bobsi.jpg', lien :'../HTML/olympien.html'},
+        {name:"Guido's Pizza",minimum:5000, price:'€€', time:'', livraison:'1500', url:'../IMAGE/guidos.jpg', lien :'../HTML/olympien.html'},
+        {name:'Sigma Burger',minimum:2500, price:'€€', time:'', livraison:'1500', url:'../IMAGE/sigma.jpg', lien :'../HTML/olympien.html'},
     ];
+
+    //Définition quartier de livraison
     $scope.quartiers = ['Quartier du fleuve', 'Quartier du fleuve - Ambassade de France', 'ACI 2000', 'Hamdallaye', 'Dar Salam',
     'Bamako Koura', 'Médina Koura', 'Bozola', 'Cité du Niger'];
+
+    //Tries des restaurants en front selon temps de livraison
     $scope.tempsLivraison = function(tiek){
         switch(tiek){
             case 'Quartier du fleuve' :
